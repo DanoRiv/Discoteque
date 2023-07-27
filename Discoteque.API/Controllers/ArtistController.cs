f2c815e91271230f0977fba1e1c5cbf31f7c56e2
DanoRiv <danio260496@gmail.com>
2023-07-26 21:05:40 -0500

    Clase 2

---
+++ Discoteque.API/Controllers/ArtistController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Discoteque.Business.IServices;
+
+namespace Discoteque.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        private readonly IArtistService _artistService;
+
+        public ArtistController(IArtistService artistService)
+        {
+            _artistService = artistService;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            var artist = await _artistService.GetArtistAsync();
+            return Ok(artist);
+        }
+    }
+}
